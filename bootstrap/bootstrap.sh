ce3c7915829ba6283fc917573ac3735f106baef8
Aaron Craelius <aaroncraelius@gmail.com>
2014-12-12 22:54:03 -0500

    Boostrap files

---
+++ bootstrap/bootstrap.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install -y git vcsh vim tmux zsh curl software-properties-common wget
+chsh -s /bin/zsh
+ 
+# git
+# sudo add-apt-repository ppa:git-core/ppa
+git config --global push.default simple
---
+++ bootstrap/java.sh
@@ -0,0 +1,4 @@
+# java
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install -y oracle-java8-installer
