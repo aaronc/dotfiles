b34f8206234619029221e41e9e58af3d5a7c91a8
Aaron Craelius <aaroncraelius@gmail.com>
2014-12-12 23:20:01 -0500

    git 2.0

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -3,5 +3,6 @@ sudo apt-get install -y git vcsh vim tmux zsh curl software-properties-common wg
 chsh -s /bin/zsh
  
 # git
-# sudo add-apt-repository -y ppa:git-core/ppa
+sudo add-apt-repository -y ppa:git-core/ppa
+sudo apt-get install -y git
 git config --global push.default simple
